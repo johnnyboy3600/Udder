f02361dd8a25059ec627cded4930364233ffba48
John Murphy <johnnyboy3600@sky.com>
2019-04-05 15:19:30 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by John Murphy on 05/04/2019.
+//  Copyright © 2019 John Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
